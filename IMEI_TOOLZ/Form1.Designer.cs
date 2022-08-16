e2dbc155e340131ce042cfd81baf5438d683302c
dkxce <25371256+dkxce@users.noreply.github.com>
2022-08-16 15:26:44 +0300

    Add files via upload

---
+++ IMEI_TOOLZ/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace IMEI_TOOLZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GENERATE:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 29);
+            this.textBox1.MaxLength = 15;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(387, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "- BAD";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 55);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(416, 120);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 4);
+            this.textBox3.MaxLength = 10;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "35480";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Generate Prefix:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Find Temp File ...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(122, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(305, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(11, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Find in Temp";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "08 3A 35 32 88 65 59 18 01"});
+            this.comboBox1.Location = new System.Drawing.Point(122, 211);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(305, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(11, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Replace in Temp";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(122, 242);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(305, 20);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(247, 7);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(182, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/dkxce/IMEIToolz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "imei-recovery-template-huawei-e173-353238856958110.exe";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(137, 300);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Find Process";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(230, 300);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Pause Process";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(331, 300);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(98, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Resume Process";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 300);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(119, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Run IMEI Recovery ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 329);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(415, 23);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "Run DC Unlocker";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 363);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMEI TOOLZ by dkxce for Huawei E173";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ IMEI_TOOLZ/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMEI_TOOLZ
+{
+    // http://alter.org.ua/ru/docs/other/imei/
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string imei = "";
+            while (!CheckIMEI(imei)) imei = GenerateImei();
+            textBox1.Text = imei;
+        }
+
+        public bool CheckIMEI(string imei)
+        {
+            if (string.IsNullOrEmpty(imei)) return false;
+            imei = imei.Trim();
+            string sub = "";
+            int total = 0;
+
+            int length = imei.Length - 1;
+            if (imei.Length == 15)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sub = ((int)char.GetNumericValue(imei[i]) * 2).ToString();
+                        total += sub.Length == 2 ? (int)char.GetNumericValue(sub[0]) + (int)char.GetNumericValue(sub[1]) : (int)char.GetNumericValue(sub[0]);
+                    }
+                    else
+                        total += ((int)char.GetNumericValue(imei[i]));
+                };
+
+                if ((int)char.GetNumericValue(imei[length]) == ((((total / 10) + 1) * 10) - total))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        public void SetIMEICheckSum(ref string imei)
+        {
+            if (string.IsNullOrEmpty(imei)) return;
+            imei = imei.Trim();
+            string sub = "";
+            int total = 0;
+
+            if (imei.Length == 14) imei += "0";
+            int length = imei.Length - 1;
+            if (imei.Length == 15)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sub = ((int)char.GetNumericValue(imei[i]) * 2).ToString();
+                        total += sub.Length == 2 ? (int)char.GetNumericValue(sub[0]) + (int)char.GetNumericValue(sub[1]) : (int)char.GetNumericValue(sub[0]);
+                    }
+                    else
+                        total += ((int)char.GetNumericValue(imei[i]));
+                };
+
+                int val = ((((total / 10) + 1) * 10) - total);
+                imei = imei.Remove(length) + val.ToString();
+            };
+        }
+
+        public string ImeiToHex(string imei)
+        {
+            if (string.IsNullOrEmpty(imei)) return "";
+            imei = imei.Trim();
+            if (imei.Length != 15) return "";
+            string res = "08 ";
+            res += imei.Substring(0,1) + "A ";
+            for (int i = 1; i < imei.Length; i += 2)
+                res += imei.Substring(i + 1, 1) + imei.Substring(i, 1) + " ";
+            return res;
+        }
+
+        public string ImeiToDec(string imei)
+        {
+            if (string.IsNullOrEmpty(imei)) return "";
+            imei = imei.Trim();
+            if (imei.Length != 15) return "";
+            string res = "08 ";
+            res += int.Parse(imei.Substring(0, 1) + "A", System.Globalization.NumberStyles.HexNumber).ToString() + " ";
+            for (int i = 1; i < imei.Length; i += 2)
+                res += int.Parse(imei.Substring(i + 1, 1) + imei.Substring(i, 1), System.Globalization.NumberStyles.HexNumber).ToString() + " ";
+            return res;
+        }
+
+        public string GenerateImei()
+        {
+            Random rnd = new Random();
+            string imei = textBox3.Text.Trim();
+            while(imei.Length < 14)
+                imei += (rnd.Next(0, 9)).ToString();
+            SetIMEICheckSum(ref imei);
+            return imei;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string imei = textBox1.Text.Trim();
+            bool isok = CheckIMEI(imei);
+            label1.Text = isok ? " - OK" : " - BAD";
+            label1.ForeColor = isok ? Color.Green : Color.Red;
+            if (!isok) return;
+            textBox2.Clear();
+
+            textBox2.Text += $"TAC: {imei.Substring(0, 2)}-{imei.Substring(2, 6)}\r\n";
+            textBox2.Text += $"SNR: {imei.Substring(8, 6)}\r\n";
+            textBox2.Text += $"CD: {imei.Substring(14)}\r\n";
+            textBox2.Text += "\r\n";
+            textBox2.Text += "NV_RAM (hex): " + ImeiToHex(imei).Trim() + "\r\n";
+            textBox2.Text += "NV_RAM (hex): " + ImeiToHex(imei).Replace(" ","") + "\r\n";
+            textBox2.Text += "NV_RAM (dec): " + ImeiToDec(imei).Trim() + "\r\n";
+            textBox2.Text += "NV_RAM (dec): " + ImeiToDec(imei).Replace(" ", "") + "\r\n";
+
+            btn34up();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = comboBox1.Items[0].ToString();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            btn34up();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btn34up();
+        }
+        private void btn34up()
+        {
+            long fl = 0;
+            long tp = 0;
+            try { fl = (new System.IO.FileInfo(textBox4.Text.Trim()).Length); } catch { };
+            button3.Enabled = (fl > 0) && (comboBox1.Text.Trim().Replace(" ", "").Length > 0) && (comboBox1.Text.Trim().Replace(" ", "").Length % 2 == 0);
+            button4.Enabled = long.TryParse(textBox5.Text.Trim().Replace(" ", ""), out tp) && (tp > 0) && (tp < fl) && CheckIMEI(textBox1.Text.Trim());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = Environment.ExpandEnvironmentVariables("%TEMP%");
+            string[] files = System.IO.Directory.GetFiles(path, "*.tmp");
+            string fn = "";
+            textBox4.Text = "";
+            foreach (string file in files)
+            {
+                System.IO.FileInfo fi = new System.IO.FileInfo(file);
+                if (fi.CreationTime < DateTime.Now.AddHours(-10)) continue;
+                if (fi.Length < 14 * 1024 * 1024) continue;
+                if (fi.Length > 16 * 1024 * 1024) continue;
+                textBox4.Text = file;
+            };
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string f = textBox4.Text.Trim();
+            try { if (!System.IO.File.Exists(f)) return; } catch { }
+
+            string t = comboBox1.Text.Trim().Replace(" ", "");
+            if (t.Length % 2 != 0) return;
+
+            byte[] toF = new byte[t.Length / 2];
+            for (int i = 0; i < toF.Length; i++)
+                toF[i] = (byte)Convert.ToInt32(t.Substring(i * 2, 2), 16);
+
+            System.IO.FileStream fs = new System.IO.FileStream(f, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            byte[] buff = new byte[16*1024*1024];
+            int pos = -1;
+            while(true)
+            {
+                int read = fs.Read(buff, 0, buff.Length);
+                if(read == 0) break;
+                int k = 0;
+                for (pos = 0; pos < read; pos++)
+                    if (buff[pos] != toF[k++]) k = 0;
+                    else if (k == toF.Length) break;
+            };
+            if (pos == fs.Length) pos = -1;
+            fs.Close();
+            textBox5.Text = pos.ToString();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            btn34up();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            btn34up();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string f = "";
+            long fl = 0;
+            long tp = 0;
+            try { fl = (new System.IO.FileInfo(f = textBox4.Text.Trim()).Length); } catch { };
+            if (fl == 0) return;
+            long.TryParse(textBox5.Text.Trim().Replace(" ", ""), out tp);
+            if (tp <= 0) return;
+            if (tp >= fl) return;            
+            string hex = ImeiToHex(textBox1.Text.Trim()).Trim().Replace(" ","");
+            byte[] toR = new byte[hex.Length / 2];
+            for (int i = 0; i < toR.Length; i++)
+                toR[i] = (byte)Convert.ToInt32(hex.Substring(i * 2, 2), 16);
+
+            System.IO.FileStream fs = new System.IO.FileStream(f, System.IO.FileMode.Open, System.IO.FileAccess.ReadWrite);
+            fs.Position = tp;
+            fs.Write(toR, 0, toR.Length);
+            fs.Close();
+            MessageBox.Show($"{toR.Length} bytes to {System.IO.Path.GetFileName(f)} has been writed", "Modify IMEI");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            foreach(System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (p.ProcessName.ToLower().StartsWith("imei-recovery"))
+                {
+                    MessageBox.Show($"Found {p.Id} as {p.ProcessName}", "Huawei E173 IMEI Recovery");
+                    return;
+                };                
+            };
+            MessageBox.Show($"Not Found Any Process", "Huawei E173 IMEI Recovery");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (p.ProcessName.ToLower().StartsWith("imei-recovery"))
+                {
+                    bool ok = ProcSleeper.SuspendProcess(p.Id);
+                    MessageBox.Show($"{p.ProcessName}\r\nProcess {p.Id} " + (ok ? " has bin Paused " : " Error"), "Huawei E173 IMEI Recovery");
+                    return;
+                };
+            };
+            MessageBox.Show($"Not Found Any Process", "Huawei E173 IMEI Recovery");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (p.ProcessName.ToLower().StartsWith("imei-recovery"))
+                {
+                    bool ok = ProcSleeper.ResumeProcess(p.Id);
+                    MessageBox.Show($"{p.ProcessName}\r\nProcess {p.Id} " + (ok ? " has bin Resumed " : " Error"), "Huawei E173 IMEI Recovery");
+                    return;
+                };
+            };
+            MessageBox.Show($"Not Found Any Process", "Huawei E173 IMEI Recovery");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try { Process.Start(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "imei-recovery-template-huawei-e173-353238856958110.exe")); } catch { };
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            try { Process.Start(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dc-unlocker2client.exe")); } catch { };
+        }
+    }
+
+    public class ProcSleeper
+    {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        private static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        private static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr handle);
+
+
+        public static bool SuspendProcess(int pid)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(pid); // throws exception if process does not exist
+                foreach (ProcessThread pT in process.Threads)
+                {
+                    IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                    if (pOpenThread == IntPtr.Zero) continue;
+                    SuspendThread(pOpenThread);
+                    CloseHandle(pOpenThread);
+                    return true;
+                };
+            }
+            catch { };
+            return false;
+        }
+
+        public static bool ResumeProcess(int pid)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(pid);
+                if (process.ProcessName == string.Empty) return false;
+                foreach (ProcessThread pT in process.Threads)
+                {
+                    IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                    if (pOpenThread == IntPtr.Zero) continue;
+                    int suspendCount = 0;
+                    do { suspendCount = ResumeThread(pOpenThread); }
+                    while (suspendCount > 0);
+                    CloseHandle(pOpenThread);
+                    return true;
+                }
+            }
+            catch { };
+            return false;
+        }
+    }
+}
